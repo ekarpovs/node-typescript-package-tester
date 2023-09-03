5395043764524699e0ea2e21ec4f5f9eb3e6384a
evgeny <ekarpovsky@gmail.com>
2023-09-03 14:50:33 +0000

    Fix jest.config

--- jest.config.ts
+++ jest.config.ts
@@ -1,8 +1,8 @@
-import type {Config} from 'jest';
-import {defaults} from 'jest-config';
-
-const config: Config = {
-  moduleFileExtensions: [...defaults.moduleFileExtensions, 'mts'],
+module.exports = {
+  preset: 'ts-jest',
+  testEnvironment: 'node',
+  transform: {
+    '^.+\\.ts?$': 'ts-jest',
+  },
+  transformIgnorePatterns: ['<rootDir>/node_modules/'],
 };
-
-export default config;
